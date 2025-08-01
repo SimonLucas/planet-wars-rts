a7dce3ae6265fea213a76b5c6b8adb8d9c846450
SimonLucas <simonmarklucas@gmail.com>
2025-08-01 14:07:47 +0100

    Update RunPartialAsServer.kt

--- app/src/main/kotlin/competition_entry/RunPartialAsServer.kt
+++ app/src/main/kotlin/competition_entry/RunPartialAsServer.kt
@@ -1,7 +1,6 @@
 package competition_entry
 
 import games.planetwars.partial.PartialGameAgentServer
-import json_rmi.GameAgentServer
 
 fun main() {
     val server = PartialGameAgentServer(port = 7080, agentClass = CarefulPartialAgent::class)
